73dc1edbd28294e6cbe6e4e517e6e324a1633174
eunhye <dmsp2377@naver.com>
2024-06-02 13:45:44 +0900

    first commit

---
+++ src/main/java/com/quest_exfo/backend/BakendApplication.java
@@ -0,0 +1,13 @@
+package com.quest_exfo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakendApplication.class, args);
+	}
+
+}
