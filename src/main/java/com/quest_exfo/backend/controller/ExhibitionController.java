5cca0dc3a1a75cfd768a2c80fe9f309a3594cee2
jolennon77 <jolennon77@gmail.com>
2024-06-04 22:53:22 +0900

    해외박람회 api 중복처리

--- src/main/java/com/quest_exfo/backend/controller/ExhibitionController.java
+++ src/main/java/com/quest_exfo/backend/controller/ExhibitionController.java
@@ -2,10 +2,12 @@ package com.quest_exfo.backend.controller;
 
 import com.quest_exfo.backend.service.ExhibitionService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/exhibition")
 public class ExhibitionController {
   private final ExhibitionService exhibitionService;
 
@@ -16,6 +18,6 @@ public class ExhibitionController {
   @GetMapping("/fetch-exhibitions")
   public String fetchExhibitions(@RequestParam(defaultValue = "23") int totalPages, @RequestParam(defaultValue = "100") int perPage) {
     exhibitionService.fetchAndSaveExhibitions(totalPages, perPage);
-    return "Exhibitions data fetched and saved successfully!";
+    return "성공데스웅";
   }
 }
--- src/main/java/com/quest_exfo/backend/service/ExhibitionService.java
+++ src/main/java/com/quest_exfo/backend/service/ExhibitionService.java
@@ -4,12 +4,14 @@ import com.quest_exfo.backend.dto.request.ExhibitionRequestDTO;
 import com.quest_exfo.backend.dto.response.ExhibitionResponseDTO;
 import com.quest_exfo.backend.entity.Exhibition;
 import com.quest_exfo.backend.repository.ExhibitionRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,21 +19,25 @@ public class ExhibitionService {
   private final ExhibitionRepository exhibitionRepository;
   private final RestTemplate restTemplate;
 
+  @Value("${exhibition.apiKey}")
+  private String apiKey;
+
   public ExhibitionService(ExhibitionRepository exhibitionRepository, RestTemplate restTemplate) {
     this.exhibitionRepository = exhibitionRepository;
     this.restTemplate = restTemplate;
   }
 
   public void fetchAndSaveExhibitions(int totalPages, int perPage) {
-    String serviceKey = "f/yYy8svm3t0PGLHgpkhrFhC6SYzwr4ZDpzGe9/6CYxCWCmGcRCh0iAeIArWF6hZHJ8CS/7NMAhWRB/V6Yjl3A==";
 
     for (int page = 1; page <= totalPages; page++) {
-      String url = String.format("https://api.odcloud.kr/api/15042123/v1/uddi:4f4295f6-40e7-48fd-9a25-cb827b97dff6?page=%d&perPage=%d&serviceKey=%s", page, perPage, serviceKey);
+      String url = String.format("https://api.odcloud.kr/api/15042123/v1/uddi:4f4295f6-40e7-48fd-9a25-cb827b97dff6?page=%d&perPage=%d&serviceKey=%s", page, perPage, apiKey);
       try {
         ResponseEntity<ExhibitionResponseDTO> responseEntity = restTemplate.getForEntity(url, ExhibitionResponseDTO.class);
         ExhibitionResponseDTO response = responseEntity.getBody();
         if (response != null) {
-          List<Exhibition> exhibitions = response.getData().stream().map(this::convertToEntity).collect(Collectors.toList());
+          List<Exhibition> exhibitions = response.getData().stream()
+              .map(this::saveOrUpdate)
+              .collect(Collectors.toList());
           exhibitionRepository.saveAll(exhibitions);
         }
       } catch (HttpClientErrorException e) {
@@ -39,6 +45,21 @@ public class ExhibitionService {
         System.err.println("오류 메시지: " + e.getResponseBodyAsString());
       }
     }
+  }
+
+  private Exhibition saveOrUpdate(ExhibitionRequestDTO dto) {
+    List<Exhibition> existingExhibitions = exhibitionRepository.findByExhibitionYearAndOrganizerNameAndFullEnglishNameAndStartDate(
+        dto.getExhibitionYear(), dto.getOrganizerName(), dto.getFullEnglishName(), dto.getStartDate());
+
+    Exhibition exhibition;
+    if (!existingExhibitions.isEmpty()) {
+      exhibition = existingExhibitions.get(0);
+      updateExhibition(exhibition, dto);
+    } else {
+      exhibition = convertToEntity(dto);
+    }
+
+    return exhibition;
   }
 
   private Exhibition convertToEntity(ExhibitionRequestDTO dto) {
@@ -60,5 +81,24 @@ public class ExhibitionService {
     exhibition.setOrganizerAddress(dto.getOrganizerAddress());
     exhibition.setOrganizerName(dto.getOrganizerName());
     return exhibition;
+  }
+
+  private void updateExhibition(Exhibition exhibition, ExhibitionRequestDTO dto) {
+    exhibition.setExhibitionField(dto.getExhibitionField());
+    exhibition.setExhibitionItem(dto.getExhibitionItem());
+    exhibition.setExhibitionBuilding(dto.getExhibitionBuilding());
+    exhibition.setExhibitionCountry(dto.getExhibitionCountry());
+    exhibition.setExhibitionYear(dto.getExhibitionYear());
+    exhibition.setExhibitionCity(dto.getExhibitionCity());
+    exhibition.setExhibitionFrequency(dto.getExhibitionFrequency());
+    exhibition.setRecruitmentStartDate(dto.getRecruitmentStartDate());
+    exhibition.setRecruitmentEndDate(dto.getRecruitmentEndDate());
+    exhibition.setStartDate(dto.getStartDate());
+    exhibition.setShortEnglishName(dto.getShortEnglishName());
+    exhibition.setFullEnglishName(dto.getFullEnglishName());
+    exhibition.setEndDate(dto.getEndDate());
+    exhibition.setKoreanName(dto.getKoreanName());
+    exhibition.setOrganizerAddress(dto.getOrganizerAddress());
+    exhibition.setOrganizerName(dto.getOrganizerName());
   }
 }
