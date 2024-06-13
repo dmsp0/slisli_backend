bb6db8a2c0c79b7747f1494719767a325aac3c3a
jolennon77 <jolennon77@gmail.com>
2024-06-13 17:53:12 +0900

    프로필 파일업로드

--- src/main/java/com/quest_exfo/backend/entity/Member.java
+++ src/main/java/com/quest_exfo/backend/entity/Member.java
@@ -32,6 +32,8 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private Role role = Role.USER;
 
+    private String ProfileImgPath;
+
     @Builder
     public Member(String email, String password, String name, Role role){
         this.email=email;
--- src/main/java/com/quest_exfo/backend/service/MemberServiceItf.java
+++ src/main/java/com/quest_exfo/backend/service/MemberServiceItf.java
@@ -6,12 +6,15 @@ import com.quest_exfo.backend.dto.request.MemberUpdateDTO;
 import com.quest_exfo.backend.dto.response.MemberResponseDTO;
 import com.quest_exfo.backend.dto.response.MemberTokenDTO;
 import com.quest_exfo.backend.entity.Member;
+import java.io.IOException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface MemberServiceItf {
     HttpStatus checkEmailDuplicate(String email);
     MemberResponseDTO signup(MemberSignupDTO memberSignupDTO);
     MemberTokenDTO login(MemberLoginDTO memberLoginDTO);
     MemberResponseDTO check(Member member, String password);
-    MemberResponseDTO update(Member member, MemberUpdateDTO updateDto);
+    MemberResponseDTO update(Member member, MemberUpdateDTO updateDto,MultipartFile file)
+        throws IOException;
 }
